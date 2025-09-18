b53bd0ab71ffafc0c73f6f67c8fe2258f71836e9
egoing <egoing@gmail.com>
2025-09-18 11:27:34 +0900

    1. 컴포넌트는 함수다. 2. 대문자로 시작한다. 3. 리턴값은 단일 태그다. 4. 이것은 tsx다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
+function Counter() {
+    return (
+        <>
+            <h1>Counter</h1>
+            <button>+2</button> 0
+        </>
+    );
+}
+
 function App() {
-    return <>Hello world</>;
+    return (
+        <>
+            <Counter></Counter>
+        </>
+    );
 }
 
 export default App;
