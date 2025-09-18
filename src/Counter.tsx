9e01f7ad08925a2f0c80c7acfecc25baa9ee93a7
egoing <egoing@gmail.com>
2025-09-18 17:51:52 +0900

    컴퍼넌트 외부와 통신하는, 결과를 예측할 수 없는 로직을 side effect라고 한다. 사이드 이팩트는 useEffect 안에서 별도로 관리한다. 첫번째 파라미터는 함수이고, 여기에 사이드 이팩트를 담는다. 두번째 파라미터는 배열이다. 생략 => 컴포넌트와 같은 랜더링 주기를 갖는다. 빈배열 => 한번 실행된다. 있다 => 원소 중에 변경된 값이 있다면 실행된다.

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 type CounterProps = {
     title: string;
@@ -12,6 +12,17 @@ export function Counter({ title, initValue }: CounterProps) {
         setStep(Number(evt.target.value));
     };
     const design = { textDecoration: "underline" };
+
+    useEffect(() => {
+        document.title = String(countValue);
+    }, [countValue]);
+
+    useEffect(() => {
+        fetch("http://localhost:9999/counter")
+            .then((data) => data.json())
+            .then((result) => countChange(result.value));
+    }, []);
+
     return (
         <>
             <h1 style={design}>{title}</h1>
