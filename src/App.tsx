82e35ae12c4f0925c97553093f1351ca9e4b2a80
egoing <egoing@gmail.com>
2025-09-18 14:34:52 +0900

    useState는 state를 만드는 함수다. 첫번째 파마미터는 초기값이다. state는 배열이다. 첫번째 원소는 state의 값이다 (읽을 때 쓴다) 두번째 원소는 함수다. (변경 할 때 쓴다) 값이 바뀌면, 컴포넌트 함수가 다시 실행된다. useState는 변경된 값을 새로운 state의 첫번째 원소로 리턴한다. 새로운 리턴값이 웹페이지에 반영된다. 끝

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 type CounterProps = {
     title: string;
     initValue: number;
 };
 function Counter({ title, initValue }: CounterProps) {
+    const count = useState(initValue);
+    const countValue = count[0];
+    const countChange = count[1];
+    function up() {
+        countChange(countValue + 1);
+    }
     return (
         <>
             <h1>{title}</h1>
-            <button>+1</button> {initValue}
+            <button onClick={up}>+1</button> {countValue}
         </>
     );
 }
@@ -16,7 +23,6 @@ function App() {
     return (
         <>
             <Counter title="카운터" initValue={10}></Counter>
-            <Counter title="불면증 카운터" initValue={20}></Counter>
         </>
     );
 }
