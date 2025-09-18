3b22554e0965b6657087e5ca80748fa55a295c2d
egoing <egoing@gmail.com>
2025-09-18 13:16:27 +0900

    1. 속성은 props라고 한다. 2. 속성은 함수의 파라미터이다. 3. 파라미터는 객체이다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 
-function Counter() {
+function Counter(props) {
+    console.log("props", props);
     return (
         <>
-            <h1>Counter</h1>
-            <button>+2</button> 0
+            <h1>{props.title}</h1>
+            <button>+1</button> {props.initValue}
         </>
     );
 }
@@ -12,7 +13,8 @@ function Counter() {
 function App() {
     return (
         <>
-            <Counter></Counter>
+            <Counter title="카운터" initValue="10"></Counter>
+            <Counter title="불면증 카운터" initValue="20"></Counter>
         </>
     );
 }
