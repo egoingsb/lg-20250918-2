42f88cde2916077379bb5332c733f120c7eaf882
egoing <egoing@gmail.com>
2025-09-19 10:42:01 +0900

    Counter 컴포넌트의 up 함수 비동기 처리 추가 및 document.title 업데이트 개선

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -7,19 +7,32 @@ type CounterProps = {
 export function Counter({ title, initValue }: CounterProps) {
     const [countValue, countChange] = useState(initValue);
     const [step, setStep] = useState(10);
-    const up = () => countChange(countValue + step);
+    const up = async () => {
+        const response = await fetch("http://localhost:9999/counter", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ value: countValue + step }),
+        });
+        const result = await response.json();
+        countChange(result.value);
+    };
     const changeHandler = (evt: React.ChangeEvent<HTMLInputElement>) => {
         setStep(Number(evt.target.value));
     };
+
     const design = { textDecoration: "underline" };
 
     useEffect(() => {
-        document.title = String(countValue);
-    }, [countValue]);
+        document.title = String(countValue) + "," + String(step);
+    }, [countValue, step]);
 
     useEffect(() => {
         fetch("http://localhost:9999/counter")
-            .then((data) => data.json())
+            .then((data) => {
+                return data.json();
+            })
             .then((result) => countChange(result.value));
     }, []);
 
